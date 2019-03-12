cbf2780816c7fa73e9d05aacd40dd5c421500a1d
Richard Luan Souza Sobreiro <richard.sobreiro@supermix.com.br>
2019-03-12 00:43:31 -0300

    hkbk

--- GeradorDadosOtimizacaoArtigoSBPO/GerarDadosAleatoriosMDVHRPTW.cs
+++ GeradorDadosOtimizacaoArtigoSBPO/GerarDadosAleatoriosMDVHRPTW.cs
@@ -9,13 +9,13 @@
             double[,] cac = new double[V, qP];
             double[] cuc = new double[qB]; // Custo de uso do caminhao k
             int ti[V][V] = ...; // Tempo trajeto ida entre i e j
-int tv[V][V] = ...; // Tempo trajeto volta entre i e j
-int tp[rP] = ...; // Tempo de pesagem em cada ponto de carga p
-        int td[V] = ...; // Tempo de descarga no cliente c
-        int hc[rC][Vg] = ...; // Hora de chega para cada viagem do cliente
-float vc[rC][Vg] = ...; // Volume de material solicitado pelo cliente em cada viagem 
-int qv[V] = ...; // Quantidade de viagens para cada cliente
-        int kp[rP] = ...; // Quantidade veiculos disponiveis por central
+            int tv[V][V] = ...; // Tempo trajeto volta entre i e j
+            int tp[rP] = ...; // Tempo de pesagem em cada ponto de carga p
+            int td[V] = ...; // Tempo de descarga no cliente c
+            int hc[rC][Vg] = ...; // Hora de chega para cada viagem do cliente
+            float vc[rC][Vg] = ...; // Volume de material solicitado pelo cliente em cada viagem 
+            int qv[V] = ...; // Quantidade de viagens para cada cliente
+            int kp[rP] = ...; // Quantidade veiculos disponiveis por central
     }
 
         static GerarDados()
