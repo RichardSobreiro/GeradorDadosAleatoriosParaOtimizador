44cf3548e61044ecd58b176120035a87fc57caa8
Richard Luan Souza Sobreiro <richard.sobreiro@supermix.com.br>
2019-03-31 23:36:38 -0300

    fdfdsfsa

--- GeradorDadosOtimizacaoArtigoSBPO/GerarDadosProgramacaoProducaoVeiculos.cs
+++ GeradorDadosOtimizacaoArtigoSBPO/GerarDadosProgramacaoProducaoVeiculos.cs
@@ -23,9 +23,29 @@ namespace GeradorDadosOtimizacaoArtigoSBPO
             FuncoesGerais.GenerateMatrixRowsByColumns(dv, 8, 40, qViagens, qPontosCarga);
             FuncoesGerais.WriteMatrixNxNToFile(file, dv, qViagens, qPontosCarga, "dv");
 
+            double[] td = new double[qViagens];
+            FuncoesGerais.GenerateRandomArray(td, 10, 20, qViagens);
+            FuncoesGerais.WriteArrayToFile(file, td, qViagens, "td");
+
+            double[] tmaxvc = new double[qViagens];
+            FuncoesGerais.GenerateRandomArray(tmaxvc, 120, 120, qViagens);
+            FuncoesGerais.WriteArrayToFile(file, tmaxvc, qViagens, "tmaxvc");
+
             double[] hs = new double[qViagens];
             FuncoesGerais.GenerateRandomArray(hs, 360, 1200, qViagens);
             FuncoesGerais.WriteArrayToFile(file, hs, qViagens, "hs");
+
+            double[,] c = new double[qViagens, qPontosCarga];
+            FuncoesGerais.GenerateMatrixRowsByColumns(c, 40, 80, qViagens, qPontosCarga);
+            FuncoesGerais.WriteMatrixNxNToFile(file, c, qViagens, qPontosCarga, "c");
+
+            double[] qvMax = new double[qPontosCarga];
+            FuncoesGerais.GenerateRandomArray(qvMax, 33, 33, qPontosCarga);
+            FuncoesGerais.WriteArrayToFile(file, qvMax, qPontosCarga, "qvMax");
+
+            double[] cuve = new double[qPontosCarga];
+            FuncoesGerais.GenerateRandomArray(cuve, 5, 10, qPontosCarga);
+            FuncoesGerais.WriteArrayToFile(file, cuve, qPontosCarga, "cuve");
 
             file.Close();
         }
